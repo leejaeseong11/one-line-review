3e541b4261484269526e18f1ac84f8a8eed92174
leejaeseong11 <easeon78@gmail.com>
2022-06-25 15:26:21 +0900

    feat: 로컬 데이터 출력

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import CreateContents from './Contents/CreateContents';
 import ReadContents from './Contents/ReadContents';
+import ReadLocalData from './Contents/ReadLocalData';
 
 function App() {
   return (
     <div className="App">
-      <ReadContents />
+      <ReadLocalData />
     </div>
   );
 }
